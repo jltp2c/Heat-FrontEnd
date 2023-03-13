019bc8c316d2be1d5956015e1d55ad105e6c98ac
Ismet Medjebeur <i.medjebeur@live.fr>
2023-03-13 12:04:02 +0100

    create and profile page modified

--- src/pages/CreateProfile/CreateProfile.jsx
+++ src/pages/CreateProfile/CreateProfile.jsx
@@ -78,6 +78,7 @@ function CreateProfile() {
             id="age"
             min={18}
             value={age}
+            pat
             onChange={(event) => setAge(event.target.value)}
           />
         </label>
