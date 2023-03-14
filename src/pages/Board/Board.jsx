22db9139e5c2a960b5570bfe95ba21693c4d70a6
Lucie-Lanssade <lucie.lanssade@gmail.com>
2023-03-14 21:24:50 +0100

    working progress bar

--- src/pages/Board/Board.jsx
+++ src/pages/Board/Board.jsx
@@ -1,40 +1,53 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
+import ReviewsBar from "../../components/Profile/ReviewsBar";
+import myApi from "../../service/service";
 
 const Board = () => {
+  const [date, setDate] = useState(() => new Date());
+  const [foodsConsumed, setfoodsConsumed] = useState([]);
 
- const [date,setDate]=useState(() => new Date())
+  const getAllFoodsConsumed = async () => {
+    try {
+      const response = await myApi.get("/api/board/foods/consumed", {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      });
+      // console.log(response.data.foodConsumed)
+      setfoodsConsumed(response.data.foodConsumed);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  // const getTodayDate = () =>{
-  //     const event = new Date();
-  //   const [today] = event.toISOString().split("T")
-  //   console.log(today);
-  //     return today
-  // }
+  useEffect(() => {
+    getAllFoodsConsumed();
+  }, [date]);
 
-const previousDate = () => {
-  const copy = new Date(date.toString())
-  copy.setDate(copy.getDate() - 1)
-  setDate(copy)
-}
+  const previousDate = () => {
+    const copy = new Date(date.toString());
+    copy.setDate(copy.getDate() - 1);
+    setDate(copy);
+  };
 
-const nextDate = () => {
-  const copy = new Date(date.toString())
-  copy.setDate(copy.getDate() + 1)
-  setDate(copy)
-}
-    
- return (
-   <div>
-     <div>
-       <button onClick={previousDate}>←</button>
-       <button onClick={()=>setDate(() => new Date())}>{date.toDateString()}</button> 
-       <button onClick={nextDate}>→</button>
-     </div>
-   </div>
- )
+  const nextDate = () => {
+    const copy = new Date(date.toString());
+    copy.setDate(copy.getDate() + 1);
+    setDate(copy);
+  };
 
-  
+  return (
+    <div>
+      <div>
+        <button onClick={previousDate}>←</button>
+        <button onClick={() => setDate(() => new Date())}>
+          {date.toDateString()}
+        </button>
+        <button onClick={nextDate}>→</button>
+      </div>
+      <div>
+        <ReviewsBar foodsConsumed={foodsConsumed}></ReviewsBar>
+      </div>
+    </div>
+  );
 };
 
-export default Board
-
+export default Board;
