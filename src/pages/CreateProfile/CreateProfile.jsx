fd81b0622b5b97ec9a7374c62204d6da4878b8fe
Lucie-Lanssade <lucie.lanssade@gmail.com>
2023-03-15 19:19:31 +0100

    createprofile

--- src/pages/CreateProfile/CreateProfile.jsx
+++ src/pages/CreateProfile/CreateProfile.jsx
@@ -11,7 +11,8 @@ function CreateProfile() {
   const [currentWeight, setCurrentWeight] = useState("");
   const [weightGoal, setWeightGoal] = useState("");
   const navigate = useNavigate();
-const {user,setUser , authenticateUser, removeToken } = useContext(AuthContext);
+  const { user, setUser, authenticateUser, removeToken } =
+    useContext(AuthContext);
 
   function getToken() {
     return localStorage.getItem("token");
@@ -28,7 +29,6 @@ const {user,setUser , authenticateUser, removeToken } = useContext(AuthContext);
       weightGoal,
     };
 
-  
     try {
       const response = await myApi.post("/api/board/profile", profileToCreate, {
         headers: { Authorization: `Bearer ${currentToken}` },
@@ -48,88 +48,91 @@ const {user,setUser , authenticateUser, removeToken } = useContext(AuthContext);
     removeToken();
     authenticateUser();
   }
- 
+
   return (
     <>
-    <form onSubmit={handleSubmit}>
       <div>
-        <option disabled value="-1">
-          Select a category
-        </option>{" "}
-        <select
-          value={gender}
-          name=""
-          id="gender"
-          onChange={(event) => setGender(event.target.value)}
-        >
-          <option disabled value="disabled">
-            Gender :
-          </option>
-          <option value="man">Man</option>
-          <option value="woman">Woman</option>
-        </select>
-     
+        <h1>New here?</h1>
+        <p>Complete your profile and start your journey right now!</p>
       </div>
+      <form className="createFormContainer" onSubmit={handleSubmit}>
+        <div className="cat">
+          <option disabled value="-1">
+            Select a category
+          </option>{" "}
+          <select
+            value={gender}
+            name=""
+            id="gender"
+            onChange={(event) => setGender(event.target.value)}
+          >
+            <option disabled value="disabled">
+              Gender :
+            </option>
+            <option value="man">Man</option>
+            <option value="woman">Woman</option>
+          </select>
+        </div>
 
-      <div>
-        <label htmlFor="age">
-          Age:&nbsp;
-          <input
-            type="number"
-            id="age"
-            min={18}
-            value={age}
-            onChange={(event) => setAge(event.target.value)}
-            placeholder="18"
-          />
-        </label>
-      </div>
+        <div className="cat">
+          <label htmlFor="age">
+            Age:&nbsp;
+            <input
+              type="number"
+              id="age"
+              min={18}
+              value={age}
+              onChange={(event) => setAge(event.target.value)}
+              placeholder="18"
+            />
+          </label>
+        </div>
 
-      <div>
-        <label htmlFor="height">
-          Height:&nbsp;
-          <input
-            type="number"
-            id="currentHeight"
-            value={currentHeight}
-            onChange={(event) => setCurrentHeight(event.target.value)}
-            placeholder="180"
-          />
-          cms
-        </label>
-      </div>
+        <div className="cat">
+          <label htmlFor="height">
+            Height:&nbsp;
+            <input
+              type="number"
+              id="currentHeight"
+              value={currentHeight}
+              onChange={(event) => setCurrentHeight(event.target.value)}
+              placeholder="180"
+            />
+            cms
+          </label>
+        </div>
 
-      <div>
-        <label htmlFor="weight">
-          Weight:&nbsp;
-          <input
-            type="number"
-            id="currentWeight"
-            value={currentWeight}
-            onChange={(event) => setCurrentWeight(event.target.value)}
-            placeholder="80"
-          />
-          kgs
-        </label>
-      </div>
+        <div className="cat">
+          <label htmlFor="weight">
+            Weight:&nbsp;
+            <input
+              type="number"
+              id="currentWeight"
+              value={currentWeight}
+              onChange={(event) => setCurrentWeight(event.target.value)}
+              placeholder="80"
+            />
+            kgs
+          </label>
+        </div>
 
-      <div>
-        <label htmlFor="weightGoal">
-          My weight goal:&nbsp;
-          <input
-            type="number"
-            id="weightGoal"
-            value={weightGoal}
-            onChange={(event) => setWeightGoal(event.target.value)}
-            placeholder="70"
-          />
-          kgs
-        </label>
-      </div>
+        <div className="cat">
+          <label htmlFor="weightGoal">
+            My weight goal:&nbsp;
+            <input
+              type="number"
+              id="weightGoal"
+              value={weightGoal}
+              onChange={(event) => setWeightGoal(event.target.value)}
+              placeholder="70"
+            />
+            kgs
+          </label>
+        </div>
 
-      <button>Create my profile </button>
-    </form>
-    <button onClick={handleLogOut}>Logout</button>
+        <button>Create my profile </button>
+      </form>
+      <button onClick={handleLogOut}>Logout</button>
     </>
   );
 }
